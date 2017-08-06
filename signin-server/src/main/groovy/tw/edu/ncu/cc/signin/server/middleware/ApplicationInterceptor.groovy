927bed790fcf03a7ce870344325eddb73aa6435e
djosix <djosicks@gmail.com>
2017-08-06 23:10:28 +0800

    Use log api

--- signin-server/src/main/groovy/tw/edu/ncu/cc/signin/server/middleware/ApplicationInterceptor.groovy
+++ signin-server/src/main/groovy/tw/edu/ncu/cc/signin/server/middleware/ApplicationInterceptor.groovy
@@ -8,10 +8,27 @@ import org.springframework.web.servlet.handler.HandlerInterceptorAdapter
 import javax.servlet.http.HttpServletRequest
 import javax.servlet.http.HttpServletResponse
 
+import java.util.HashMap
+import java.util.Map
+import java.text.SimpleDateFormat
+import java.util.Date
+import org.springframework.http.HttpEntity
+import org.springframework.http.HttpHeaders
+import org.springframework.http.MediaType
+import org.springframework.http.ResponseEntity
+import org.springframework.util.MultiValueMap
+import org.springframework.util.LinkedMultiValueMap
+import org.springframework.web.client.RestTemplate
+import org.springframework.web.servlet.ModelAndView
+import org.springframework.beans.factory.annotation.Value
+
 @Component
 public class ApplicationInterceptor extends HandlerInterceptorAdapter {
 
     private Logger logger = LoggerFactory.getLogger( this.getClass() )
+
+    @Value( '${log-api.uri}' )
+    private String logApiUri
 
     @Override
     public boolean preHandle( HttpServletRequest request, HttpServletResponse response, Object handler ) throws Exception {
@@ -23,6 +40,45 @@ public class ApplicationInterceptor extends HandlerInterceptorAdapter {
                 )
         )
         return true
+    }
+
+    @Override
+    public void postHandle( HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView ) throws Exception {
+        def message = String.format(
+                "`%s: %s %s %s %s`",
+                "signin_service",
+                new SimpleDateFormat( "yyyy-MM-dd HH:mm:ss" ).format( new Date() ),
+                request.remoteAddr,
+                request.method,
+                getRequestPath( request )
+        )
+        if( !callLogAPI( message, logApiUri ) ) {
+            logger.error( "Unable to call log api properly" )
+        }
+    }
+
+    private static boolean callLogAPI( String message, String logApiUri ) {
+
+        // Headers
+        def headers = new HttpHeaders()
+        headers.setContentType( MediaType.APPLICATION_FORM_URLENCODED )
+
+        // POST parameters
+        def map = new LinkedMultiValueMap<String, String>()
+        map.add( "application", "signin_service" )
+        map.add( "message", message )
+
+        // Request
+        def request = new HttpEntity<MultiValueMap<String, String>>( map, headers )
+
+        try {
+            // Make a request
+            def response = new RestTemplate().postForEntity( logApiUri, request, String.class )
+            return true
+
+        } catch ( Exception e ) {
+            return false
+        }
     }
 
     private static String getRequestPath( HttpServletRequest request ) {
